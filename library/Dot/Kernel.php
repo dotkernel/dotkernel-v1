557c39a2e25be7002051e8cdaa65ad489da9c827
teo <teo@432dea6f-c1b7-4361-9aab-92109e4501db>
2009-11-16 16:13:56 +0000

    Updated DotKernel version number

--- library/Dot/Kernel.php
+++ library/Dot/Kernel.php
@@ -37,7 +37,7 @@ class Dot_Kernel
     /**
      * Dot Kernel version identification
      */
-    const VERSION = '1.0.4';    
+    const VERSION = '1.0.6';    
 	/**
 	 * Constructor
 	 * @access public
