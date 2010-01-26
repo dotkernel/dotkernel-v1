54294e11de0eb5fa9a0c8591ea920069b0246d97
teo <teo@432dea6f-c1b7-4361-9aab-92109e4501db>
2010-01-26 14:43:54 +0000

    Update version to 1.1.0

--- trunk/library/Dot/Kernel.php
+++ trunk/library/Dot/Kernel.php
@@ -37,7 +37,7 @@ class Dot_Kernel
     /**
      * Dot Kernel version identification
      */
-    const VERSION = '1.0.7';    
+    const VERSION = '1.1.0';    
 	/**
 	 * Constructor
 	 * @access public
