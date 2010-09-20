e06983326d8d27133bb3b5fbc3dd997c74d2441e
teo <teo@432dea6f-c1b7-4361-9aab-92109e4501db>
2010-09-20 09:55:40 +0000

    Fixed - # 52: Admin hide debug bar in Login page http://www.dotkernel.net/view.php?id=52 [Tracker ID: #52]

--- controllers/admin/IndexController.php
+++ controllers/admin/IndexController.php
@@ -82,8 +82,8 @@ $tpl->displayMessage();
 $tpl->parse('MAIN_CONTENT', 'tpl_main');
 
 // show debugbar 
-if(TRUE == $config->settings->admin->debugbar && 
-	($requestController!= 'admin' && $requestAction!='login'))
+if(TRUE == $config->settings->admin->debugbar &&
+   ($requestController!= 'admin' && $requestAction!='login'))
 {
 	$debug = new Dot_Debug($db, $tpl);
 	$debug->startTimer = $startTime;
