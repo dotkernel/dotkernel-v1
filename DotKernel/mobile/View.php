41cf3808207b07afd4b560baf0d31a29d0ca52d2
teo <teo@432dea6f-c1b7-4361-9aab-92109e4501db>
2010-11-04 06:36:52 +0000

    Fixed - # 90: DotKernel upgrade from 1.3.0 to 1.3.1 http://www.dotkernel.net/view.php?id=90 [Tracker ID: #90]

--- DotKernel/mobile/View.php
+++ DotKernel/mobile/View.php
@@ -4,7 +4,7 @@
 * DotKernel Application Framework
 *
 * @category   DotKernel
-* @package    Frontend
+* @package    Mobile
 * @copyright  Copyright (c) 2009-2010 DotBoost Technologies Inc. Canada (http://www.dotboost.com)
 * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 * @version    $Id$
@@ -14,7 +14,7 @@
 * View Model
 * abstract over the Dot_Template class
 * @category   DotKernel
-* @package    Frontend
+* @package    Mobile
 * @author     DotKernel Team <team@dotkernel.com>
 */
 
