d85896abce93438cafd21f890a40cf9160736f0c
adi <adi@432dea6f-c1b7-4361-9aab-92109e4501db>
2011-03-24 11:52:21 +0000

    a lot of code for models is the same in the admin and frontend. models should extend the same class that provides common code to all modules

---
+++ library/Dot/Model.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* DotBoost Technologies Inc.
+* DotKernel Application Framework
+*
+* @category   DotKernel
+* @package    DotLibrary
+* @copyright  Copyright (c) 2009-2011 DotBoost Technologies Inc. Canada (http://www.dotboost.com)
+* @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+* @version    $Id: Acl.php 403 2011-01-24 12:35:06Z teo $
+*/
+
+/**
+* Control user access in DotKernel application
+* access control list (ACL)
+* @category   DotKernel
+* @package    DotLibrary
+* @author     DotKernel Team <team@dotkernel.com>
+*/
+
+class Dot_Model
+{
+	/**
+	 * Dot_Model constructor
+	 * @access public
+	 * @return Dot_Model
+	 */
+	public function __construct()
+	{
+		$this->db = Zend_Registry::get('database');
+	}
+}
---
+++ library/Dot/Model/User.php
@@ -0,0 +1,73 @@
+<?php
+/**
+* DotBoost Technologies Inc.
+* DotKernel Application Framework
+*
+* @category   DotKernel
+* @package    DotLibrary
+* @copyright  Copyright (c) 2009-2011 DotBoost Technologies Inc. Canada (http://www.dotboost.com)
+* @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+* @version    $Id: Smtp.php 397 2011-01-13 12:56:27Z teo $
+*/
+
+/**
+* Alternate SMTP server mail() class 
+* @category   DotKernel
+* @package    DotLibrary
+* @subpackage DotModel
+* @author     DotKernel Team <team@dotkernel.com>
+*/
+
+class Dot_Model_User extends Dot_Model
+{
+	/**
+	 * Email constructor
+	 * @access public 
+	 * @return Dot_Model_User
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->option = Zend_Registry::get('option');
+	}
+	/**
+	 * Get user by field
+	 * @access public
+	 * @param string $field
+	 * @param string $value
+	 * @return array
+	 */
+	public function getUserBy($field = '', $value = '')
+	{
+		$select = $this->db->select()
+					   ->from('user')
+					   ->where($field.' = ?', $value)
+					   ->limit(1);
+		$result = $this->db->fetchRow($select);
+		return $result;
+	}
+	/**
+	 * Update user
+	 * @access public
+	 * @param array $data
+	 * @return void
+	 */
+	public function updateUser($data)
+	{
+		$id = $data['id'];
+		unset ($data['id']);
+		$this->db->update('user', $data, 'id = '.$id);
+	}
+	/**
+	 * Add new user
+	 * @access public
+	 * @param array $data
+	 * @return void
+	 */
+	public function addUser($data)
+	{		
+		// if you want to add an inactive user, un-comment the below line, default: isActive = 1
+		// $data['isActive'] = 0;
+		$this->db->insert('user',$data);
+	}
+}
