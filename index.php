36c7bec4b80195cb083648f132b586041adc8a45
adi <adi@432dea6f-c1b7-4361-9aab-92109e4501db>
2011-03-24 13:19:56 +0000

    moved initialization code to DotKernel::initialize

--- index.php
+++ index.php
@@ -20,19 +20,16 @@
 $startTime = microtime();
 
 // Define application environment
-defined('APPLICATION_ENV') || 
-	define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 // Define application path	
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__)));
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__)));
 
 //Set error reporting
 if(APPLICATION_ENV != 'production') error_reporting(-1);
 
 //Set include  path to library directory
-set_include_path(
-	implode(PATH_SEPARATOR, array(realpath(dirname(__FILE__).'/library'), get_include_path())));
+set_include_path(implode(PATH_SEPARATOR, array(realpath(dirname(__FILE__).'/library'), get_include_path())));
 
 // Define PATH's (absolute paths)  to configuration, controllers, DotKernel, templates  directories
 defined('CONFIGURATION_PATH') || define('CONFIGURATION_PATH', realpath(dirname(__FILE__).'/configs'));
@@ -51,110 +48,15 @@ $zend_loader = Zend_Loader_Autoloader::getInstance();
 //includes all classes in library folder. That class names must start with Dot_
 $zend_loader->registerNamespace('Dot_');
 
-// Create registry object, as read-only object to store there config, settings, and database
-$registry = new Zend_Registry(array(), ArrayObject::ARRAY_AS_PROPS);
-Zend_Registry::setInstance($registry);
+// initialize the DotKernel Enviromnment
+$registry = Dot_Kernel::initialize();
 
-//Load configuration settings from application.ini file and store it in registry
-$config = new Zend_Config_Ini(CONFIGURATION_PATH.'/application.ini', APPLICATION_ENV);
-$registry->configuration = $config;
+// Pass controll to the controller
+//Dot_kernel::gallop();
 
-//Load routes(modules, controllers, actions) settings from router.xml file and store it in registry
-$router = new Zend_Config_Xml(CONFIGURATION_PATH.'/router.xml');
-$registry->router = $router;
-
-// Create  connection to database, as singleton , and store it in registry
-$db = Zend_Db::factory('Pdo_Mysql', $config->database->params->toArray());
-$registry->database = $db;
-
-//Load specific configuration settings from database, and store it in registry
-$settings = Dot_Settings::getSettings();
-$registry->settings = $settings;
-
-//Set PHP configuration settings from application.ini file
-Dot_Settings::setPhpSettings($config->phpSettings->toArray());
-
-// Start Index Controller
-
-$realRequest = substr($_SERVER['REQUEST_URI'], strlen(dirname($_SERVER['PHP_SELF'])));
-$getRequest = '?' . http_build_query($_GET);
-$tmpRequest = str_replace($getRequest, '', $realRequest);
-// remove the GET param from url - not to take in consideration when spliting the url request
-// into module - controller - action 
-$requestRaw = explode('/', trim($tmpRequest, '/'));
-
-// We are in frontend or in other module ? Prebuilt modules: frontend, admin, rss, ...
-$requestModule = 'frontend';
-if (in_array($requestRaw['0'], $config->resources->modules->toArray()))
-{
-	$requestModule = strtolower(basename(stripslashes($requestRaw['0'])));
-}
-// if  we are NOT in frontend  module
-if ($requestModule != 'frontend')
-    array_shift($requestRaw);
-    
-// set Controller and Action value, default Index
-$requestController = 'Index';
-if (isset($requestRaw['0']) && $requestRaw['0'] != '')
-{
-	$requestController = strtolower(basename(stripslashes($requestRaw['0'])));
-}
-
-// set Action value, default nothing
-$requestAction = '';
-if (isset($requestRaw['1']) && $requestRaw['1'] != '')
-{
-	$requestAction = strtolower(basename(stripslashes($requestRaw['1'])));
-}
-else
-{	//take the default action from router.xml
-	$requestAction = $registry->router->routes->action->{$requestModule}->{ucfirst($requestController)};
-}
-
-// we have extra variables, so we load all in the global array $request
-$request = array();
-while (list($key, $val) = each($requestRaw))
-{
-	$request[$val] = current($requestRaw);
-	next($requestRaw);
-}
-
-// remove first element of the request array, is module and action in it
-array_shift($request);
-
-//memory request into param variable and load them into registry
-$route = array();
-$route['module'] = $requestModule;
-$route['controller'] = $requestController;
-$route['action'] = $requestAction;
-$route = array_merge($route, $request);
-$registry->route = $route;
-
-// initialize default options for dots that may be overwritten
-$option = Dot_Settings::getOptionVariables($route['module'], 'default');
-$registry->option = $option;
-
-// initialize the session
-Dot_Session::start();
-$session = Zend_Registry::get('session');
-
-// set seo routes and initialize seo options
-$seo = new Dot_Seo();
-$seo->routes();
-$registry->seo = $seo->getOption();
-
-// instantiate Dot_Acl used as access control layer
-$dotAcl = new Dot_Acl();
-
-//set the roles for the current module
-$dotAuth = Dot_Auth::getInstance();
-$dotAuth->setRoles($dotAcl->getRoles());
-
-/**
-*  From this point , the control is taken by the Front Controller
-*  call the Front Controller specific file, but check first if exists 
-*/
-
+/*
+ * From this point , the control is taken by the Front Controller
+ * call the Front Controller specific file, but check first if it exists 
+ */
 $frontControllerPath = CONTROLLERS_PATH.'/'.$requestModule.'/'.'IndexController.php';
-
 !file_exists($frontControllerPath) ?  Dot_Kernel::pageNotFound() :  require($frontControllerPath);
--- library/Dot/Kernel.php
+++ library/Dot/Kernel.php
@@ -25,6 +25,129 @@ class Dot_Kernel
 	 * @var string
 	 */
 	const VERSION = '1.4.0 dev';
+	public static function gallop()
+	{
+		// these variables need to be accessible outside of this function, so they need to be global
+		global $registry, $requestModule, $config, $dotAuth, $db, $startTime;
+		/*
+		 * From this point , the control is taken by the Front Controller
+		 * call the Front Controller specific file, but check first if it exists 
+		 */
+		$frontControllerPath = CONTROLLERS_PATH.'/'.$requestModule.'/'.'IndexController.php';
+		!file_exists($frontControllerPath) ?  Dot_Kernel::pageNotFound() :  require($frontControllerPath);
+	}
+	/**
+	 * Initialize the global variables 
+	 */
+	public static function initialize()
+	{
+		// these variables need to be accessible outside of this function, so they need to be global
+		global $requestModule, $requestAction, $requestController, $config, $dotAuth, $db, $request, $session;
+
+		// Create registry object, as read-only object to store there config, settings, and database
+		$registry = new Zend_Registry(array(), ArrayObject::ARRAY_AS_PROPS);
+		Zend_Registry::setInstance($registry);
+
+		//Load configuration settings from application.ini file and store it in registry
+		$config = new Zend_Config_Ini(CONFIGURATION_PATH.'/application.ini', APPLICATION_ENV);
+		$registry->configuration = $config;
+
+		//Load routes(modules, controllers, actions) settings from router.xml file and store it in registry
+		$router = new Zend_Config_Xml(CONFIGURATION_PATH.'/router.xml');
+		$registry->router = $router;
+
+		// Create  connection to database, as singleton , and store it in registry
+		$db = Zend_Db::factory('Pdo_Mysql', $config->database->params->toArray());
+		$registry->database = $db;
+
+		//Load specific configuration settings from database, and store it in registry
+		$settings = Dot_Settings::getSettings();
+		$registry->settings = $settings;
+
+		//Set PHP configuration settings from application.ini file
+		Dot_Settings::setPhpSettings($config->phpSettings->toArray());
+
+		// Start Index Controller
+
+		$realRequest = substr($_SERVER['REQUEST_URI'], strlen(dirname($_SERVER['PHP_SELF'])));
+		$getRequest = '?' . http_build_query($_GET);
+		$tmpRequest = str_replace($getRequest, '', $realRequest);
+		// remove the GET param from url - not to take in consideration when spliting the url request
+		// into module - controller - action
+		$requestRaw = explode('/', trim($tmpRequest, '/'));
+
+		// We are in frontend or in other module ? Prebuilt modules: frontend, admin, rss, ...
+		$requestModule = 'frontend';
+		if (in_array($requestRaw['0'], $config->resources->modules->toArray()))
+		{
+			$requestModule = strtolower(basename(stripslashes($requestRaw['0'])));
+		}
+		// if we are NOT in the frontend  module
+		if ($requestModule != 'frontend')
+		{
+			array_shift($requestRaw);
+		}
+
+		// set Controller and Action value, default Index
+		$requestController = 'Index';
+		if (isset($requestRaw['0']) && $requestRaw['0'] != '')
+		{
+			$requestController = strtolower(basename(stripslashes($requestRaw['0'])));
+		}
+
+		// set Action value, default nothing
+		$requestAction = '';
+		if (isset($requestRaw['1']) && $requestRaw['1'] != '')
+		{
+			$requestAction = strtolower(basename(stripslashes($requestRaw['1'])));
+		}
+		else
+		{
+			//take the default action from router.xml
+			$requestAction = $registry->router->routes->action->{$requestModule}->{ucfirst($requestController)};
+		}
+
+		// we have extra variables, so we load all in the global array $request
+		$request = array();
+		while (list($key, $val) = each($requestRaw))
+		{
+			$request[$val] = current($requestRaw);
+			next($requestRaw);
+		}
+
+		// remove first element of the request array, is module and action in it
+		array_shift($request);
+
+		//memory request into param variable and load them into registry
+		$route = array();
+		$route['module'] = $requestModule;
+		$route['controller'] = $requestController;
+		$route['action'] = $requestAction;
+		$route = array_merge($route, $request);
+		$registry->route = $route;
+
+		// initialize default options for dots that may be overwritten
+		$option = Dot_Settings::getOptionVariables($route['module'], 'default');
+		$registry->option = $option;
+
+		// initialize the session
+		Dot_Session::start();
+		$session = Zend_Registry::get('session');
+
+		// set seo routes and initialize seo options
+		$seo = new Dot_Seo();
+		$seo->routes();
+		$registry->seo = $seo->getOption();
+
+		// instantiate Dot_Acl used as access control layer
+		$dotAcl = new Dot_Acl();
+
+		//set the roles for the current module
+		$dotAuth = Dot_Auth::getInstance();
+		$dotAuth->setRoles($dotAcl->getRoles());
+
+		return $registry;
+	}
 	/**
 	 * End the execution of the application,
 	 * by sending an 404 header and redirecting to home page
