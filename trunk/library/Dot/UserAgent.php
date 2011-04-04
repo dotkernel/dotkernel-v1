5cda4c0029ebb8c4a8a785a891e0535403ce73ce
julian <julian@432dea6f-c1b7-4361-9aab-92109e4501db>
2011-04-04 10:24:06 +0000

    testing svn:keywords

--- trunk/library/Dot/UserAgent.php
+++ trunk/library/Dot/UserAgent.php
@@ -7,7 +7,7 @@
 * @package    DotLibrary
 * @copyright  Copyright (c) 2009-2011 DotBoost Technologies Inc. Canada (http://www.dotboost.com)
 * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-* @version    $Id:$
+* @version    $Id$
 */
 
 /**
