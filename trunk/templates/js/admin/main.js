d4e6c40e6ac448ceb57195efc6c9a39aad08bcd2
adi <adi@432dea6f-c1b7-4361-9aab-92109e4501db>
2011-04-22 14:00:55 +0000

    added orange_round

--- trunk/templates/js/admin/main.js
+++ trunk/templates/js/admin/main.js
@@ -1,8 +1,9 @@
 /*** menu ***/
+var debug = false;		// set to true to disable menu closing
 // prevent pollution of the global scope, wrap everything inside a function
 (function(){
 	var timeout = 500,	// ms before the menu is closed
-		closeTimer,			// pointer to the timer that closes the popup
+		closeTimer,			// timer that closes the popup
 		selectedItem;		// the popup that is currently being shown
 
 	// hide the submenu	
@@ -18,7 +19,6 @@
 	{
 		// bind mouseover and mouseout events to all items of the main menu
 		$('#mainMenu > li')
-
 			.bind('mouseover', function(){
 				// clear the close timer if it's set
 				if (closeTimer){
@@ -28,15 +28,17 @@
 				// close any other menu that might already be shown
 				closeMenu();
 				// show the submenu
-				selectedItem = $(this).find('ul').eq(0).css('visibility', 'visible');
-				// add a menuHove class to the main menu item, to keep its style
+				selectedItem = $(this).find('ul').css('visibility', 'visible');
 				$(this).children().first().addClass('menuHover');
 			})
 
 			.bind('mouseout',  function(){
-				// when the mouse leaver the menu, wait <timeout> milliseconds
-				// and then close it automatically 
-				closeTimer = window.setTimeout(closeMenu, timeout);
+				if (!debug)
+				{
+					// when the mouse leaves the menu, wait <timeout> milliseconds
+					// and then close it automatically 
+					closeTimer = window.setTimeout(closeMenu, timeout);
+				}
 			});
 	})
 	// attatch a click event to the document to close the menu
