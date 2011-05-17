cc998621cbaadd90dda52b19767dce3fe6b2433b
adi <adi@432dea6f-c1b7-4361-9aab-92109e4501db>
2011-05-17 11:46:53 +0000

    Crons with CLI [Tracker ID: #121]

---
+++ trunk/Console/Newsletter.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* DotBoost Technologies Inc.
+* DotKernel Application Framework
+*
+* @category   DotKernel
+* @package    CLI
+* @copyright  Copyright (c) 2009-2011 DotBoost Technologies Inc. Canada (http://www.dotboost.com)
+* @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+* @version    $Id: Smtp.php 397 2011-01-13 12:56:27Z teo $
+*/
+
+/**
+* Use Model Class 
+* @category   DotKernel
+* @package    Console
+* @subpackage Newsletter
+* @author     DotKernel Team <team@dotkernel.com>
+*/
+
+class Console_Newsletter extends Dot_Model
+{
+	/**
+	 * Email constructor
+	 * @access public 
+	 * @return Dot_Model_User
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
---
+++ trunk/Console/User.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* DotBoost Technologies Inc.
+* DotKernel Application Framework
+*
+* @category   DotKernel
+* @package    CLI
+* @copyright  Copyright (c) 2009-2011 DotBoost Technologies Inc. Canada (http://www.dotboost.com)
+* @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+* @version    $Id: Smtp.php 397 2011-01-13 12:56:27Z teo $
+*/
+
+/**
+* Use Model Class 
+* @category   DotKernel
+* @package    Console
+* @subpackage User
+* @author     DotKernel Team <team@dotkernel.com>
+*/
+
+class Console_User extends Dot_Model_User
+{
+	/**
+	 * Email constructor
+	 * @access public 
+	 * @return Dot_Model_User
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function countUsers()
+	{
+		$select = $this->db->select()
+			->from('user', array('cnt' => 'count(id)'));
+		$result = $this->db->fetchRow($select);
+		return $result['cnt'];
+	}
+}
