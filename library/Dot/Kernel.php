293b08a5379103086174eefa8555f4e9b58b9b52
julian <22009710+arhimede@users.noreply.github.com>
2012-06-08 01:28:59 +0000

    changed version number to 1.6.1 dev

--- library/Dot/Kernel.php
+++ library/Dot/Kernel.php
@@ -24,7 +24,7 @@ class Dot_Kernel
 	 * Dot Kernel version identification
 	 * @var string
 	 */
-	const VERSION = '1.6.0';
+	const VERSION = '1.6.1 dev';
 	/*
  	 * Start DotKernel
 	 * Pass controll to the Front Controller if it exists,
