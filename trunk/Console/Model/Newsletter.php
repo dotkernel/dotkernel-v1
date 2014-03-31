c2ffa132c9459e628d8a04eccc749298d5f45bc9
julian <julian@432dea6f-c1b7-4361-9aab-92109e4501db>
2014-03-31 15:21:06 +0000

    update release number to 1.7.1

--- trunk/Console/Model/Newsletter.php
+++ trunk/Console/Model/Newsletter.php
@@ -1,16 +1,16 @@
 <?php
 /**
-* DotBoost Technologies Inc.
-* DotKernel Application Framework
-*
-* @category   DotKernel
-* @package    CLI
-* @subpackage Newsletter
+ * DotBoost Technologies Inc.
+ * DotKernel Application Framework
+ *
+ * @category   DotKernel
+ * @package    CLI
+ * @subpackage Newsletter
  * @copyright  Copyright (c) 2009-2014 DotBoost Technologies Inc. (http://www.dotboost.com)
-* @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-* @version    $Id$
-* @author     DotKernel Team <team@dotkernel.com>
-*/
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version    $Id$
+ * @author     DotKernel Team <team@dotkernel.com>
+ */
 
 class Console_Model_Newsletter extends Dot_Model
 {
--- trunk/library/Dot/Kernel.php
+++ trunk/library/Dot/Kernel.php
@@ -24,7 +24,7 @@ class Dot_Kernel
 	 * Dot Kernel version identification
 	 * @var string
 	 */
-	const VERSION = '1.7.1 dev';
+	const VERSION = '1.7.1';
 	/**
  	 * Start DotKernel
 	 * Pass controll to the Front Controller if it exists,
