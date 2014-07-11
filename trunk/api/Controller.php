bac020d60a392150192e6d50f51185b153453aba
costin <costin@432dea6f-c1b7-4361-9aab-92109e4501db>
2014-07-11 14:11:45 +0000

    Api files and changes in application.ini [Tracker ID: #267]

---
+++ trunk/api/Controller.php
@@ -0,0 +1,48 @@
+<?php 
+/**
+ * DotBoost Technologies Inc.
+ * DotKernel Application Framework
+ *
+ * @category   DotKernel
+ * @package    CLI
+ * @copyright  Copyright (c) 2009-2014 DotBoost Technologies Inc. (http://www.dotboost.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version    $Id$
+ * @author     DotKernel Team <team@dotkernel.com>
+ */
+
+switch ($registry->action)
+{
+	case 'version':
+		$data = array();
+		$data[] = array('result' => 'ok');
+		$data[] = array('response' => Dot_Kernel::VERSION);
+		$jsonString = Zend_Json::encode($data);
+		echo $jsonString;
+	break;
+	
+	case 'opCache':
+		if ($registry->configuration->api->params->key == $registry->arguments['key'])
+		{
+			$opCacheModel = new Api_Model_OpCache();
+			echo $opCacheModel->opCacheStatus();
+		}
+		else
+		{
+			header("HTTP/1.0 401 Unauthorized");
+			$data = array();
+			$data[] = array('result' => 'error');
+			$data[] = array('response' => "Invalid Key");
+			$jsonString = Zend_Json::encode($data);
+			echo $jsonString;
+		}
+	break;
+
+	default:
+		$data = array();
+		$data[] = array('result' => 'error');
+		$data[] = array('response' => "Action doesn't exist");
+		$jsonString = Zend_Json::encode($data);
+		echo $jsonString;
+	break;
+}
