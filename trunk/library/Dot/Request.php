92de4ae58dc075a6346ea11abf213d0db2c4d222
gabi <gabi@432dea6f-c1b7-4361-9aab-92109e4501db>
2014-11-24 14:41:21 +0000

    Removed UserAgent and HTTP_REFFER from index.php Created new class: Dot_Request It will hold $_SERVER, $_GET and $_POST data
    [Tracker ID: #0000285]

---
+++ trunk/library/Dot/Request.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * DotBoost Technologies Inc.
+ * DotKernel Application Framework
+ *
+ * @category   DotKernel
+ * @package    DotLibrary
+ * @copyright  Copyright (c) 2009-2014 DotBoost Technologies Inc. (http://www.dotboost.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version    $Id$
+ */
+
+/**
+ * Process the Request Data
+ * @category   DotKernel
+ * @package    DotLibrary
+ * @subpackage DotRequest
+ * @author     DotKernel Team <team@dotkernel.com>
+ */
+class Dot_Request
+{
+	protected static $_server;
+	protected static $_get; 
+	protected static $_post;
+	private static $_isDataSet = false ;
+	/**
+	 * Sets the Server Request Parameters
+	 * CAUTION !!! Use this only once
+	 * Teoretically it shouldn't change if the  
+	 * @param array $server $_SERVER
+	 * @param array $get $_GET
+	 * @param array $post $_POST
+	 */
+	public static function setRequestData($server, $get = array(), $post = array())
+	{
+		if(self::$_isDataSet != true )
+		{
+			self::$_server = $server;
+			self::$_get  = $get;
+			self::$_post = $post;
+			self::$_isDataSet = true;
+		}
+		else // prevent calling second time 
+		{
+			trigger_error('Request Data is already set', E_USER_NOTICE );
+		}
+	}
+	
+	public static function getRequestData()
+	{
+		return array(
+			'server' => self::$_server,
+			'get' => self::$_get,
+			'post' => self::$_post,
+		);
+	}
+	
+	/******* Frequently Called Functions ********/
+	
+	/**
+	 * Get the Request User Agent
+	 * 
+	 * This function was implemented because
+	 * it is called frequently 
+	 * 
+	 * @access public 
+	 * @return string $userAgent
+	 */
+	public static function getUserAgent()
+	{
+		if(isset(self::$_server['HTTP_USER_AGENT']))
+			return self::$_server['HTTP_USER_AGENT'];
+		return '';
+	}
+	
+	/**
+	 * Get the Request User Agent
+	 *
+	 * This function was implemented because
+	 * it is called frequently
+	 *
+	 * @access public
+	 * @return string $userAgent
+	 */
+	public static function getHttpReffer()
+	{
+		if(isset(self::$_server['HTTP_REFERER']))
+			return self::$_server['HTTP_REFERER'];
+		return '';
+	}
+}
