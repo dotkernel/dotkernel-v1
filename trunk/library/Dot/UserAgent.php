c30ba30f3f11e686febe59912b7db24e50be4d3f
gabi <gabi@432dea6f-c1b7-4361-9aab-92109e4501db>
2014-11-27 17:52:42 +0000

    Removed WURFL Cloud Integration Removed Dot_UserAgent layers Currently Using Dot_UserAgent_Utilities Will soon implement UA Sniffer and UA Device Detector System [Tracker ID: #0000284]

--- trunk/library/Dot/UserAgent.php
+++ trunk/library/Dot/UserAgent.php
@@ -20,150 +20,75 @@
 class Dot_UserAgent
 {
 	/**
+	 * Config Array
+	 * @var array $config
+	 * @access private
+	 */
+	private $_config;
+
+	/**
+	 * User Agent Detector
+	 * 
+	 * And a good illustration of static class members 
+	 * 
+	 * @var Dot_UserAgent_Base
+	 * @access private 
+	 * @static
+	 */
+	private static $_detector;
+	
+	/**
 	 * Constructor,
 	 * @access public
 	 * @return Dot_UserAgent
 	*/
-	function __construct()
+	public function __construct()
 	{
-		$this->config = Zend_Registry::get('configuration');
+		$this->_config = Zend_Registry::get('configuration');
 	}
 
 	/**
-	 * Get HTTP Request Device Info from WURFL
+	 * Get HTTP Request Device Info from UA Detector
 	 * Device may be Bot, Checker, Console, Desktop, Email, Feed, Mobile, Offline, Probe, Spam, Text, Validator
 	 * @return object
 	 */
-	public static function getDeviceInfo($httpRequest)
+	public function getDeviceInfo($httpRequest)
 	{
-		$config = Zend_Registry::get('configuration');
-		if(Dot_UserAgent::checkWurflApi() && empty($config->resources->useragent->wurflcloud->active))
+		if(self::$_detector === null)
 		{
-			$wurfl = Dot_UserAgent_Wurfl::getInstance();
-			$deviceInfo = $wurfl->getDevice($httpRequest['HTTP_USER_AGENT']);
+			return array();
 		}
-		else
-		{
-			$wurfl = Dot_UserAgent_WurflCloud::getInstance();
-			$deviceInfo = $wurfl->getDevice($httpRequest);
-		}
-		return $deviceInfo;
+		self::$_detector->getDeviceInfo($httpRequest);
 	}
 	
 	/**
-	 * Test if Wurfl package is installed and working
+	 * 
+	 */
+	public function createDetector($config = null, $detectorName = 'Null')
+	{
+		if($detectorName == 'Null')
+		{
+			return new Dot_UserAgent_Null(null);
+		}
+		$className = 'Dot_UserAgent_'.$detectorName;
+		if(class_exists($className))
+		{
+			self::$_detector = new $className($config);
+		}
+	}
+	
+	/**
+	 * Check the Detector Status
+	 * Tests if the Detector package is installed and working
 	 * @return boolean
 	 */
-	public static function checkWurflApi()
+	public function checkApi()
 	{
-		$config = Zend_Registry::get('configuration');
-		$enabled = FALSE;
-		if(isset($config->resources->useragent->wurflapi->lib_dir) && isset($config->resources->useragent->wurflapi->config_file))
+		if(self::$_detector === null)
 		{
-			if(class_exists('Dot_UserAgent_Wurfl') && file_exists(CONFIGURATION_PATH.'/useragent/wurfl.xml'))
-			{
-				if(file_exists($config->resources->useragent->wurflapi->lib_dir . 'Application.php'))
-				{
-					$enabled = TRUE;
-				}
-			}
+			return false;
 		}
-		return $enabled;
+		return self::$_detector->checkApi();
 	}
 
-	/**
-	 * Return the name of the browser icon based on User Agent
-	 * @access public
-	 * @static
-	 * @param string $userAgent
-	 * @return string
-	 */
-	public static function getBrowserIcon($userAgent, $return = 'icon')
-	{
-		$xml = new Zend_Config_Xml(CONFIGURATION_PATH.'/useragent/browser.xml');
-		$browser = $xml->name->type->toArray();
-		foreach ($browser as $key => $val)
-		{
-			if (stripos($userAgent,$val['uaBrowser']) !== FALSE)
-			{
-				if('browser' == $return)
-				{
-					return $val['uaBrowser'];
-				}
-				return $val['uaIcon'];
-			}
-		}
-		return 'unknown';
-	}
-
-	/**
-	 * Return the name of the OS icon based on User Agent
-	 * @access public
-	 * @static
-	 * @param string $userAgent
-	 * @return array
-	 */
-	public static function getOsIcon($userAgent)
-	{
-		$xml = new Zend_Config_Xml(CONFIGURATION_PATH.'/useragent/os.xml');
-		$os = $xml->type->toArray();
-		foreach ($os as $major)
-		{
-			foreach ($major as $osArray)
-			{
-				if(array_key_exists('identify', $osArray))
-				{//there are minor version
-				// if we have only one menu, Zend_Config_Xml return a simple array, not an array with key 0(zero)
-				if (!array_key_exists('0', $osArray['identify']))
-				{
-					//we create the array with key 0
-					$osIdentify[] = $osArray['identify'];
-				}
-				else
-				{
-					$osIdentify = $osArray['identify'];
-				}
-					foreach ($osIdentify as $minor)
-					{
-						//check if there are different strings for detecting an operating system
-						if(strstr($minor['uaString'],'|') !== FALSE)
-						{
-							$uaStringArray = explode('|',$minor['uaString']);
-							foreach ($uaStringArray as $uaString)
-							{
-								if ((stripos($userAgent, $uaString) !== false))
-								{
-									$operatingSystem = array( 'icon'=>strtolower(str_replace(' ', '_', $osArray['os'])),
-																						'major'=>$osArray['os'],
-																						'minor'=>$minor['osName']);
-									return $operatingSystem;
-								}
-							}
-						}
-						else
-						{
-							if ((stripos($userAgent, $minor['uaString']) !== false))
-							{
-								$operatingSystem = array( 'icon'=>strtolower(str_replace(' ', '_', $osArray['os'])),
-																					'major'=>$osArray['os'],
-																					'minor'=>$minor['osName']);
-								return $operatingSystem;
-							}
-						}
-					}
-				}
-				else
-				{//no minor version known for this os
-					if ((stripos($userAgent, $osArray['os']) !== false))
-					{
-						$operatingSystem = array( 'icon'=>strtolower(str_replace(' ', '_', $osArray['os'])),
-																			'major'=>$osArray['os'],
-																			'minor'=>'');
-						return $operatingSystem;
-					}
-				}
-			}
-		}
-		return array('icon'=>'unknown', 'major'=>'', 'minor'=>'');
-	}
 }
