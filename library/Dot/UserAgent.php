b75643328575864fb912f8eddc45a4f86ee14b14
Gabi DJ <git@gabisuciu.ro>
2014-12-02 16:48:17 +0000

    Removed Dot_UserAgent implementation But still left an empty class to mantain the architecture and avoid possible autoloader trouble [Tracker ID: #0000284]

--- library/Dot/UserAgent.php
+++ library/Dot/UserAgent.php
@@ -11,7 +11,7 @@
 */
 
 /**
-* Extract all informations from User Agent , including interaction with Luca's Passani WURFL library
+* Extract all informations from User Agent, including interaction with Luca Passani's WURFL library
 * @category   DotKernel
 * @package    DotLibrary
 * @author     DotKernel Team <team@dotkernel.com>
@@ -19,76 +19,6 @@
 
 class Dot_UserAgent
 {
-	/**
-	 * Config Array
-	 * @var array $config
-	 * @access private
-	 */
-	private $_config;
-
-	/**
-	 * User Agent Detector
-	 * 
-	 * And a good illustration of static class members 
-	 * 
-	 * @var Dot_UserAgent_Base
-	 * @access private 
-	 * @static
-	 */
-	private static $_detector;
-	
-	/**
-	 * Constructor,
-	 * @access public
-	 * @return Dot_UserAgent
-	*/
-	public function __construct()
-	{
-		$this->_config = Zend_Registry::get('configuration');
-	}
-
-	/**
-	 * Get HTTP Request Device Info from UA Detector
-	 * Device may be Bot, Checker, Console, Desktop, Email, Feed, Mobile, Offline, Probe, Spam, Text, Validator
-	 * @return object
-	 */
-	public function getDeviceInfo($httpRequest)
-	{
-		if(self::$_detector === null)
-		{
-			return array();
-		}
-		self::$_detector->getDeviceInfo($httpRequest);
-	}
-	
-	/**
-	 * 
-	 */
-	public function createDetector($config = null, $detectorName = 'Null')
-	{
-		if($detectorName == 'Null')
-		{
-			return new Dot_UserAgent_Null(null);
-		}
-		$className = 'Dot_UserAgent_'.$detectorName;
-		if(class_exists($className))
-		{
-			self::$_detector = new $className($config);
-		}
-	}
-	
-	/**
-	 * Check the Detector Status
-	 * Tests if the Detector package is installed and working
-	 * @return boolean
-	 */
-	public function checkApi()
-	{
-		if(self::$_detector === null)
-		{
-			return false;
-		}
-		return self::$_detector->checkApi();
-	}
-
+	// this is an empty class 
+	// the new Dot_UserAgent class will be implemented in DotKernel 1.9.0 (LTS)    
 }
