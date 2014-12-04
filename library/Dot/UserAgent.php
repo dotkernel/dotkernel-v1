27bc1f94b197895580e7560c9821255b3ee24050
Gabi DJ <git@gabisuciu.ro>
2014-12-04 17:34:37 +0000

    comments change - keeping it tidy

--- library/Dot/UserAgent.php
+++ library/Dot/UserAgent.php
@@ -5,13 +5,17 @@
 *
 * @category   DotKernel
 * @package    DotLibrary
- * @copyright  Copyright (c) 2009-2014 DotBoost Technologies Inc. (http://www.dotboost.com)
+* @copyright  Copyright (c) 2009-2014 DotBoost Technologies Inc. (http://www.dotboost.com)
 * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 * @version    $Id$
 */
 
 /**
 * Extract all informations from User Agent, including interaction with Luca Passani's WURFL library
+* 
+* This is an empty class 
+* The new Dot_UserAgent class will be implemented in DotKernel 1.9.0 (LTS)
+* 
 * @category   DotKernel
 * @package    DotLibrary
 * @author     DotKernel Team <team@dotkernel.com>
@@ -19,6 +23,5 @@
 
 class Dot_UserAgent
 {
-	// this is an empty class 
-	// the new Dot_UserAgent class will be implemented in DotKernel 1.9.0 (LTS)    
+
 }
