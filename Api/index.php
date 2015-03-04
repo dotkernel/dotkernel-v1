c9c6d97474fca1d149ac362eecc1224ddba9960b
Gabi DJ <git@gabisuciu.ro>
2015-03-04 16:47:31 +0000

    Implemented API with simple rate limit mechanism [Tracker ID: #0000295]

--- Api/index.php
+++ Api/index.php
@@ -48,17 +48,12 @@ Zend_Registry::setInstance($registry);
 $config = new Zend_Config_Ini(CONFIGURATION_PATH.'/application.ini', APPLICATION_ENV);
 $registry->configuration = $config;
 
-// Create  connection to database, as singleton , and store it in registry
-$db = Zend_Db::factory('Pdo_Mysql', $config->database->params->toArray());
-$registry->database = $db;
-
-// Load specific configuration settings from database, and store it in registry
-$settings = Dot_Settings::getSettings();
-$registry->settings = $settings;
-$registry->option = array();
-
-// Set PHP configuration settings from application.ini file
-Dot_Settings::setPhpSettings($config->phpSettings->toArray());
+// check if api enabled from application.ini 
+if($registry->configuration->api->params->enable != true)
+{
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
 
 // Get the action and the other arguments
 $params = array();
@@ -70,5 +65,55 @@ if (!empty($params))
 	$registry->arguments = $params;
 }
 
+// check the API key
+if($registry->configuration->api->params->key != $registry->arguments['key'])
+{
+	header("HTTP/1.0 401 Unauthorized");
+	$data = array();
+	$data[] = array('result' => 'error');
+	$data[] = array('response' => "Invalid Key");
+	$jsonString = Zend_Json::encode($data);
+	echo $jsonString;
+	exit;
+}
+
+// check rate limit 
+if($registry->configuration->api->params->lifetime == strtolower('minute'))
+{
+	$timeKey = date("i");
+	$ttl = 60;
+}
+else
+{
+	// h - 12 hrs ; H - 24 hrs 
+	$timeKey = date("H");
+	$ttl = 3600;
+}
+
+$cacheRateKey = $registry->configuration->cache->namespace. '_'. $registry->configuration->api->params->prefix .'_' .$timeKey ; 
+// using apcu directly
+// for more info about the caching layer see    http://www.dotkernel.com/tag/dotkernel-caching/
+$rate = (int)(apc_fetch($cacheRateKey));
+$rateLimit = $registry->configuration->api->params->rate_limit ;
+if($rate > $rateLimit)
+{
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+apc_store($cacheRateKey,1+$rate, $ttl);
+
+#if(apc_fetch($registry->configuration->cache->namespace))
+
+// Create  connection to database, as singleton , and store it in registry
+$db = Zend_Db::factory('Pdo_Mysql', $config->database->params->toArray());
+$registry->database = $db;
+
+// Load specific configuration settings from database, and store it in registry
+$settings = Dot_Settings::getSettings();
+$registry->settings = $settings;
+$registry->option = array();
+
+// Set PHP configuration settings from application.ini file
+Dot_Settings::setPhpSettings($config->phpSettings->toArray());
 
 include('Controller.php');
