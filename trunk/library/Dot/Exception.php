0744063a9634e1fc17ca880ee080c7e51f8202fd
gabi <gabi@432dea6f-c1b7-4361-9aab-92109e4501db>
2015-04-24 15:17:03 +0000

    added Dot_Exception for use when needed [Tracker ID: #0000297]

---
+++ trunk/library/Dot/Exception.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * DotBoost Technologies Inc.
+ * DotKernel Application Framework
+ *
+ * @category   DotKernel
+ * @package    DotLibrary
+ * @copyright  Copyright (c) 2009-2015 DotBoost Technologies Inc. (http://www.dotboost.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version    $Id: Acl.php 919 2015-04-16 15:11:00Z gabi $
+ */
+
+/**
+ * DotKernel Main Exception
+ * 
+ * This kind of Exception will be used for DotKernel related errors
+ *
+ * @category   DotKernel
+ * @package    DotLibrary
+ * @subpackage DotException
+ * @author     DotKernel Team <team@dotkernel.com>
+ */
+
+class Dot_Exception extends Exception
+{
+	
+}
