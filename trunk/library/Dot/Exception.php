4f2d8f4e3fe32fef8faf15c8a2810f4ffc6da840
gabi <gabi@432dea6f-c1b7-4361-9aab-92109e4501db>
2015-04-28 13:41:22 +0000

    Added svn properties and docblocs

--- trunk/library/Dot/Exception.php
+++ trunk/library/Dot/Exception.php
@@ -7,7 +7,7 @@
  * @package    DotLibrary
  * @copyright  Copyright (c) 2009-2015 DotBoost Technologies Inc. (http://www.dotboost.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @version    $Id: Acl.php 919 2015-04-16 15:11:00Z gabi $
+ * @version    $Id$
  */
 
 /**
