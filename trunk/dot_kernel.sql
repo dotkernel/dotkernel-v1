dda16d245775bedfdd4cae96db0a2dccb4ccafdc
gabi <gabi@432dea6f-c1b7-4361-9aab-92109e4501db>
2015-05-19 10:53:41 +0000

    updated sql file

--- trunk/dot_kernel.sql
+++ trunk/dot_kernel.sql
@@ -62,25 +62,6 @@ CREATE TABLE IF NOT EXISTS `adminLogin` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `emailTransporter`
---
-
-CREATE TABLE IF NOT EXISTS `emailTransporter` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` varchar(100) NOT NULL,
-  `pass` varchar(100) NOT NULL,
-  `server` varchar(100) NOT NULL DEFAULT 'smtp.gmail.com',
-  `port` int(5) NOT NULL DEFAULT '465',
-  `ssl` enum('tls','ssl') NOT NULL DEFAULT 'tls',
-  `capacity` int(11) NOT NULL DEFAULT '2000',
-  `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `counter` int(11) NOT NULL DEFAULT '0',
-  `isActive` enum('1','0') NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
--- --------------------------------------------------------
-
---
 -- Table structure for table `statisticVisit`
 --
 
@@ -178,7 +159,6 @@ INSERT INTO `setting` (`id`, `key`, `value`, `title`, `comment`, `isEditable`, `
 (2, 'devEmails', 'team@dotkernel.com', 'Developer Emails', 'developer emails, for debug purpose, separated by comma', '0', 'textarea', ''),
 (3, 'timeFormatShort', '%d %b %Y', 'Short Date/Time Format.', '%d - day of the month as a decimal number (range 01 to 31) %b - abbreviated month name according to the current locale %B - full month name according to the current locale %m - month as a decimal number (range 01 to 12) %y - year as a decimal number without a century (range 00 to 99) %Y - year as a decimal number including the century', '1', 'option', '%d %b %Y;%d %B %Y;%d %B %y;%d %m %Y;%d %m %y;%B %d, %Y;%b %d, %Y'),
 (4, 'timeFormatLong', '%b %d, %Y, %H:%M', 'Long Date/Time Format.', 'Date/time format, including hours, minutes and seconds', '1', 'option', '%d %b %Y, %H:%M;%d %B %Y, %H:%M;%d %B %y, %H:%M;%d %m %Y, %H:%M;%d %m %y, %H:%M;%B %d, %Y, %H:%M;%b %d, %Y, %H:%M'),
-(5, 'smtpAddresses', 'aol.com;aim.com;comcast.net;hotmail.com;earthlink.net;juno.com;juno.net;bellsouth.net;cox.net;roadrunner.com;sbcglobal.net', 'Email servers where we need to use external SMTP in order to send emails.', '', '0', 'textarea', ''),
 (7, 'resultsPerPage', '5', 'Default results per page', 'How many records will be on every page, if is not specified otherwise by a specific configuration value', '1', 'option', '5;10;20;30;40;50'),
 (8, 'whoisUrl', 'http://whois.domaintools.com', '', 'Whois lookup and Domain name search', '0', 'textarea', ''),
 (9, 'paginationStep', '3', 'Pagination Step', 'The maximum number of pages that are shown on either side of the current page in the pagination header.', '1', 'option', '3;4;5;6;7;8;9;10');
